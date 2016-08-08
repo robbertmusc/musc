5ff6368731d9a175ca0f9833ef215e9d5197cdb1
Chris Rutte <chrisrutte@gmail.com>
2016-08-08 15:36:40 +0200

    make it better

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.allow_concurrency = false
+  # toegevoegd met heroku gepiel
+
 end
