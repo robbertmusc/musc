5a5fb96cd8bfc7e09ce8030327d38e390862ba90
Chris Rutte <chrisrutte@gmail.com>
2016-08-09 17:01:22 +0200

    make it better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < Admin::BaseController
+class UsersController < ApplicationController
 # class UsersController < ApplicationController (dit was de regel voor heroku gepiel)
 	def show
 		@user = User.find(params[:id])
