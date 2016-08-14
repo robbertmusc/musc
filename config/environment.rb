90f8818931bab89c5a24b995b4c179d3fdd01f0d
Chris Rutte <chrisrutte@gmail.com>
2016-08-14 11:20:00 +0200

    plaatjes

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+# obv SO voor rmagick
+config.gem "rmagick",
+:lib => "RMagick" 
+
 # Initialize the Rails application.
 Rails.application.initialize!
